64774cafe0c71710769107659701435710ed800f
rishikherde <rishikherde8@gmail.com>
2025-03-06 19:00:54 +0530

    changed yml and py

--- transform.py
+++ transform.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import sys
+import os
 
 input_file = sys.argv[1]
-output_file = sys.argv[2]
 
 df = pd.read_csv(input_file)
 
 for col in df.select_dtypes(include=['object']).columns:
     df[col] = df[col].str.upper()
 
+output_file = os.path.join("data", "transformed.csv")
 df.to_csv(output_file, index=False)
+print(f"File created at: {output_file}")
